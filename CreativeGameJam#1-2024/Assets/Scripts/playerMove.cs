b92e7cf7e044d52229b42e67475e2a2f37a7a829
Alvin-Tulud <alvintulud@gmail.com>
2024-03-01 15:42:17 -0800

    player movement

--- CreativeGameJam#1-2024/Assets/Scripts/playerMove.cs
+++ CreativeGameJam#1-2024/Assets/Scripts/playerMove.cs
@@ -7,15 +7,36 @@ public class playerMove : MonoBehaviour
     // Start is called before the first frame update
 
     Rigidbody2D rb;
+    canJump jump;
+    public float jumpForce;
+    public float speed;
 
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        jump = GetComponentInChildren<canJump>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //left right
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = speed * Vector2.left;
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = speed * Vector2.right;
+        }
+
+        //jumping
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
+        {
+            if (jump.GetComponent<canJump>().jumpState())
+            {
+                rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+            }
+        }
     }
 }
