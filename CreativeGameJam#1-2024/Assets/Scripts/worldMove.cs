94238f825db751fdcaac7c9fd4ae8a6b8dc628df
Alvin-Tulud <alvintulud@gmail.com>
2024-03-02 13:55:20 -0800

    move the level

--- CreativeGameJam#1-2024/Assets/Scripts/worldMove.cs
+++ CreativeGameJam#1-2024/Assets/Scripts/worldMove.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class worldMove : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody2D rb;
+    public float speed;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.MovePosition(transform.position + -transform.right * speed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.MovePosition(transform.position + transform.right * speed);
+        }
+        else if (Input.GetKey(KeyCode.W) ||  Input.GetKey(KeyCode.Space))
+        {
+            rb.MovePosition(transform.position + transform.up * speed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.MovePosition(transform.position + -transform.up * speed);
+        }
     }
 }
